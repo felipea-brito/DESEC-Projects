3b5c154c13308e6499dc7437eab7cd1b5ddc26cc
felipea-brito <felipea.work@gmail.com>
2026-07-14 16:11:29 -0300

    DNS Recon

--- DNSRecon.sh
+++ DNSRecon.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-echo "Type the domain to verify: "
-read domain
+read -p "Is it a reverse or direct search, type 1 or 2: " verify
 
-for word in $(cat n0kovo_subdomains.txt);
-do
-  host $word.$domain | grep -v "NXDOMAIN"
-done
+if [[ $verify == 2 ]]; then
+
+  read -p "Type the domain to verify: " domain
+  
+  for word in $(cat n0kovo_subdomains.txt);
+  do
+    host $word.$domain | grep -v "NXDOMAIN"
+  done
+
+elif [[ $verify == 1 ]]; then
+  
+  read -p "Type the IP do verify: " ip
+  read -p "Type the range to verify: " range
+  
+  for range in $(seq "$range");
+  do
+    host -t ptr $ip.$range | grep -v "$ip"
+  done
+else
+  echo "Inform correct option."
+fi
