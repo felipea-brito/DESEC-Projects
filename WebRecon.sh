4b3e3d6581b0113cbe4e9c4834932fb90b299d41
felipea-brito <felipea.work@gmail.com>
2026-07-15 15:41:05 -0300

    Web Recon

---
+++ WebRecon.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+read -p "Type the domain: " domain
+read -p "Type one for direcotry research and two for a file [1] [2]: " option
+
+  if [[ $option == 1 ]]; then
+    
+    for word in $(cat list.txt);
+    
+    do
+      response=$(curl -s -H "User-Agent: DesecTool" -o /dev/null -w "%{http_code}" "$domain/$word/")
+    
+      if [[ $response == "200" ]]; then
+        echo "Directory discovered: $domain/$word"
+      fi
+    done
+    
+  elif [[ $option == 2 ]]; then
+
+    read -p "Type the extension: " extension
+    
+    for word in $(cat list.txt);
+    
+    do
+      response=$(curl -s -H "User-Agent: DesecTool" -o /dev/null -w "%{http_code}" "$domain/$word.$extension")
+    
+      if [[ $response == "200" ]]; then
+        echo "File discovered: $word.$extension"
+      fi
+    done
+
+  else
+    echo "Don't have this option"
+  fi
