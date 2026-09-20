085211f20175d0253451918edd86bb106fa59308
felipea-brito <felipea.work@gmail.com>
2026-09-20 14:56:59 -0400

    SubdomainTakeover.sh

--- SubdomainTakeover.sh
+++ SubdomainTakeover.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for word in $(cat '/usr/share/wordlists/seclists/Discovery/DNS/subd>
+for word in $(cat '/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt');
 do
     host -t cname $word.$1 | grep "alias for"
 done
